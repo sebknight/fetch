b18a044f7f7e68607ae16b7974f3368138e1e32a
sebknight <sebastianknightmaddox@gmail.com>
2020-02-03 01:45:20 +1300

    reduce garbage output, restrict image size

--- js/script.js
+++ js/script.js
@@ -1,27 +1,21 @@
 const button = document.getElementById('fetch');
-const picDiv = document.getElementById('picture');
-
-const dogReq = new Request('https://dog.ceo/api/breeds/image/random');
-
-// const getBreed = data => {
-//   const path = data.message;
-//   const pathArr = path.split('/');
-//   const breedIndex = pathArr[4];
-//   const breed = breedIndex.replace('-', ' ');
-// }
+const imgContainer = document.getElementById('image-container');
+const img = document.getElementById('image');
+const title = document.getElementById('title');
+const article = document.getElementById('article');
 
 const getImage = data => {
   const path = data.message;
-  const img = document.createElement('img');
   img.src = path;
-  picDiv.appendChild(img)
 }
 
 const getFacts = data => {
   const path = data.message;
+  // Break down the response to extract the breed name
   const pathArr = path.split('/');
   const breedIndex = pathArr[4];
   const breed = breedIndex.replace('-', '%20');
+  // Query the Wikipedia API for the breed
   fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${breed}%20dog&format=json&origin=*`, {
     method: 'GET',
   })
@@ -29,12 +23,22 @@ const getFacts = data => {
     return res.json();
   })
   .then((data) => {
+    // Get the snippet, scrub it of markup, 
+    // then extract the first sentence
+    // and replace the full stop
     const snippet = data.query.search[0].snippet;
     const cleanSnippet = snippet.replace(/<(?:.|\n)*?>/gm, '');
-    console.log(cleanSnippet);
+    const firstSentence = `${cleanSnippet.substring(0, cleanSnippet.indexOf('.'))}.`;
+    // Reduces garbage output
+    firstSentence.length > 15 ?
+      article.textContent = firstSentence :
+      article.textContent = "What kind of dog is that? A good one!"  
   })
 }
 
+const dogReq = new Request('https://dog.ceo/api/breeds/image/random');
+
+// Query the Dog API!
 button.addEventListener('click', () => {
   fetch(dogReq)
   .then((res) => {
@@ -45,6 +49,6 @@ button.addEventListener('click', () => {
     getFacts(data);
   })
   .catch(() => {
-    console.log('Error')
+    console.log(`Error`)
   })
 });
