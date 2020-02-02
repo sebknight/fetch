2462342a32f452c421b12a80eb803f92774410d8
sebknight <sebastianknightmaddox@gmail.com>
2020-02-03 00:45:35 +1300

    fix CORS bug

--- js/script.js
+++ js/script.js
@@ -22,14 +22,16 @@ const getFacts = data => {
   const pathArr = path.split('/');
   const breedIndex = pathArr[4];
   const breed = breedIndex.replace('-', '%20');
-  fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${breed}%20dog&format=json&prop=extracts`, {
+  fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${breed}%20dog&format=json&origin=*`, {
     method: 'GET',
   })
   .then((res) => {
-    return res;
+    return res.json();
   })
   .then((data) => {
-    console.log(data.query.search[0].snippet);
+    const snippet = data.query.search[0].snippet;
+    const cleanSnippet = snippet.replace(/<(?:.|\n)*?>/gm, '');
+    console.log(cleanSnippet);
   })
 }
 
