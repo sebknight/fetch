f9791f6dab1b5fd9e6d5be9ed2d97b34118083c5
sebknight <sebastianknightmaddox@gmail.com>
2020-02-03 00:20:21 +1300

    begin rewrite, es6 and fetch

--- js/script.js
+++ js/script.js
@@ -1,39 +1,49 @@
+const button = document.getElementById('fetch');
+const picDiv = document.getElementById('picture');
 
-function getDog() {
-    $.getJSON("https://dog.ceo/api/breeds/image/random", function (data) {
-        console.log(data);      
-        var path = data.message;
-        var pathArray = path.split("/");
-        console.log(pathArray);
-        var breedIndex = pathArray["4"];
-        var breed = breedIndex.replace("-"," ");
-        // $("#title").text(breed);  
-        $("#picture").html(JSON.stringify(data, null, 4));
-        $("#picture").html("<img src='" + path + "'>");  
-           function getWiki() {
-               $.ajax({
-                   url: '//en.wikipedia.org/w/api.php',
-                   data: {
-                       action: 'query',
-                       list: 'search',
-                       srsearch: breed + " dog",
-                       format: 'json',
-                       formatversion: 2
-                   },
-                   dataType: 'jsonp',
-                   success: function (x) {
-                       console.log(x);                       
-                       var breedSnippet = x.query.search[0].snippet;
-                       var cleanSnippet = breedSnippet.replace(/<(?:.|\n)*?>/gm, '');
-                       $("#title").text(x.query.search[0].title);
-                       $("#article").text(cleanSnippet);                    
-                   }
-               });
-           }
-        getWiki();
-    });
+const dogReq = new Request('https://dog.ceo/api/breeds/image/random');
+
+// const getBreed = data => {
+//   const path = data.message;
+//   const pathArr = path.split('/');
+//   const breedIndex = pathArr[4];
+//   const breed = breedIndex.replace('-', ' ');
+// }
+
+const getImage = data => {
+  const path = data.message;
+  const img = document.createElement('img');
+  img.src = path;
+  picDiv.appendChild(img)
 }
 
-$('#fetch').click(function () {
-    getDog();
+const getFacts = data => {
+  const path = data.message;
+  const pathArr = path.split('/');
+  const breedIndex = pathArr[4];
+  const breed = breedIndex.replace('-', '%20');
+  fetch(`http://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${breed}&format=json`, {
+    method: 'GET',
+    // headers: headers
+  })
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+}
+
+button.addEventListener('click', () => {
+  fetch(dogReq)
+  .then((res) => {
+      return res.json();
+  })
+  .then((data) => {
+    getImage(data);
+    getFacts(data);
+  })
+  .catch(() => {
+    console.log('Error')
+  })
 });
