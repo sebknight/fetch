36eb9e2187c1278b290eeb656ab0020e8b5c7a57
sebknight <sebastianknightmaddox@gmail.com>
2020-02-07 20:28:09 +1300

    implementing loader, moving to BEM

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
-const button = document.getElementById('fetch');
-const imgContainer = document.getElementById('image-container');
-const img = document.getElementById('image');
-const title = document.getElementById('title');
-const article = document.getElementById('article');
+const button = document.querySelector('.btn');
+const imgContainer = document.querySelector('.content__image-container');
+const img = document.querySelector('.content__image');
+const title = document.querySelector('.content__title');
+const article = document.querySelector('.content__article');
+const loader = document.querySelector('.content__loader');
+
+const showLoader = () => {
+  img.style.display = 'none';
+  loader.style.visibility = 'visible';
+}
+
+const hideLoader = () => {
+  loader.style.visibility = 'hidden';
+  img.style.display = 'block';
+}
 
 const getImage = data => {
+  img.onload = hideLoader();
   const path = data.message;
   img.src = path;
 }
@@ -14,6 +26,7 @@ const getFacts = data => {
   // Break down the response to extract the breed name
   const pathArr = path.split('/');
   const breedIndex = pathArr[4];
+  // Replace any dashes in the breed name with a space
   const breed = breedIndex.replace('-', '%20');
   // Query the Wikipedia API for the breed
   fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${breed}%20dog&format=json&origin=*`, {
@@ -27,12 +40,23 @@ const getFacts = data => {
     // then extract the first sentence
     // and replace the full stop
     const snippet = data.query.search[0].snippet;
-    const cleanSnippet = snippet.replace(/<(?:.|\n)*?>/gm, '');
-    const firstSentence = `${cleanSnippet.substring(0, cleanSnippet.indexOf('.'))}.`;
+
+    // Not ideal to use innerHTML, but should be OK 
+    // because it only returns text content.
+    // Faster than using a regex and accounts for escaped characters
+    const cleanSnippet = () => {
+      if (snippet) {
+        const div = document.createElement('div');
+        div.innerHTML = snippet;
+        return div.textContent;
+      }
+    }
+    
+    const firstSentence = `${cleanSnippet().substring(0, cleanSnippet().indexOf('.'))}.`;
     // Reduces garbage output
-    firstSentence.length > 15 ?
+    firstSentence.length > 10 ?
       article.textContent = firstSentence :
-      article.textContent = "What kind of dog is that? A good one!"  
+      article.textContent = 'What kind of dog is that? A good one!' 
   })
 }
 
@@ -42,9 +66,10 @@ const dogReq = new Request('https://dog.ceo/api/breeds/image/random');
 button.addEventListener('click', () => {
   fetch(dogReq)
   .then((res) => {
-      return res.json();
-  })
-  .then((data) => {
+    showLoader();
+    return res.json();
+    })
+    .then((data) => {
     getImage(data);
     getFacts(data);
   })
